3024b675fe80915f324d3f5a9a6aebed675b5b2b
Audehhhh <107974427+Audehhhh@users.noreply.github.com>
2022-09-01 23:56:49 +0700

    Add files via upload

---
+++ konversi satuan panjang/desimeter.py
@@ -0,0 +1,24 @@
+print("-------------------------------------------------------")
+print("----------------- KONVERSI SATUAN DM ------------------")
+print("-------------------------------------------------------")
+
+dm = float(input("masukan satuan dm :"))
+ulang = dm
+
+dm2mm = (dm * 100) 
+print("hasil konversi ke mm :", dm2mm, "mm")
+
+dm2cm = (dm * 10)
+print("hasil konversi ke cm :", dm2cm, "cm")
+
+dm2m = (dm / 10)
+print("hasil konversi ke m :", dm2m, "m")
+
+dm2dam = (dm / 100)
+print("hasil konversi ke dam :", dm2dam, "dam")
+
+dm2hm = (dm / 1000)
+print("hasil konversi ke hm :", dm2hm, "hm")
+
+dm2km = (dm / 10000)
+print("hasil konversi ke km :", dm2km, "km")
---
+++ konversi satuan panjang/hektometer.py
@@ -0,0 +1,24 @@
+print("-------------------------------------------------------")
+print("----------------- KONVERSI SATUAN HM ------------------")
+print("-------------------------------------------------------")
+
+hm = float(input("masukan satuan hm :"))
+ulang = hm
+
+hm2mm = (hm * 100000) 
+print("hasil konversi ke mm :", hm2mm, "mm")
+
+hm2cm = (hm * 10000)
+print("hasil konversi ke cm :", hm2cm, "cm")
+
+hm2dm = (hm * 1000)
+print("hasil konversi ke dm :", hm2dm, "dm")
+
+hm2m = (hm * 100)
+print("hasil konversi ke m :", hm2m, "m")
+
+hm2dam = (hm * 10)
+print("hasil konversi ke dam :", hm2dam, "dam")
+
+hm2km = (hm / 10)
+print("hasil konversi ke km :", hm2km, "km")
